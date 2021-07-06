4c81c0c2527d2f222d5b9caa03c715eb1ce7510b
Garrett Honeycutt <code@garretthoneycutt.com>
2021-07-06 16:22:39 -0400

    Add script to install brew and some default software

---
+++ brew_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# apg
+brew tap jzaleski/homebrew-jzaleski
+brew install apg
+
+brew install bash-completion
+brew install brew-cask-completion
+brew install bundler-completion
+brew install cask
+brew install colordiff
+brew install coreutils
+brew install cowsay
+brew install curl
+brew install direnv
+brew install git
+brew install gnupg
+brew install jq
+brew install nmap
+brew install pssh
+brew install telnet
+brew install tree
+brew install vim
+brew install watch
+brew install wget
